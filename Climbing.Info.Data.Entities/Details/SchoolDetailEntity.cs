71adfb69f1166b5c5b0d01fcc0a9b2dfee660336
JordiPijuan <jordipijuan@informaticos.com>
2016-12-21 22:04:28 +0100

    Add entities

---
+++ Climbing.Info.Data.Entities/Details/SchoolDetailEntity.cs
@@ -0,0 +1,29 @@
+﻿namespace Climbing.Info.Data.Entities.Details
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Principals;
+
+    public class SchoolDetailEntity
+    {
+
+        public long SchoolDetailId { get; set; }
+
+        [StringLength(300, ErrorMessage = "SchoolDetailName debe tener 300 caracteres o menos")]
+        public string SchoolDetailName { get; set; }
+
+        [StringLength(500, ErrorMessage = "SchoolDetailDescription debe tener 500 caracteres o menos")]
+        public string SchoolDetailDescription { get; set; }
+
+        public long SchoolId { get; set; }
+
+        public virtual SchoolEntity School { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? UpdatedDate { get; set; }
+
+    }
+}
